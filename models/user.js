58de549b3052a926743bab26bb098a833f6354de
Abubakarmunir712 <Abubakarmunir712@gmail.com>
2024-06-07 22:18:27 +0500

    User model added

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+const {isEmail} = require('validator')
+const bcrypt = require('bcrypt')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        unique:true,
+        validate: [isEmail,'Please provide a valid email']
+    },
+    password:{
+        type:String,
+        required:[true,'Password is required'],
+        minlength:[8,'Password must be at least 8 characters long']
+    },
+    verified:{
+        type:Boolean,
+        default:false
+    }
+},{timestamps:true})
+
+
+userSchema.pre('save',async function(doc,next){
+const salt = await bcrypt.genSalt();
+this.password = await bcrypt.hash(this.password,salt)
+
+})
+
+module.exports = mongoose.model('User',userSchema)
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const  express= require('express')
 const cookieParser = require('cookie-parser')
 const app =express()
 const path = require('path')
+const mongoose = require('mongoose')
+const User = require('./models/user')
+const user = require('./models/user')
 
 require('dotenv').config()
 
@@ -11,3 +14,15 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
 
+const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.urmucvs.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.connect(dbURI)
+.then((result)=>{
+
+    console.log("Connected to the database") 
+    app.listen(process.env.PORT || 3000)
+})
+.catch((err)=>{
+    console.log("Err",err)
+})
+
+ 
